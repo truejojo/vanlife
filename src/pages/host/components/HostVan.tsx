2e0066b812736b8a9bac4ab96540cccb4addc01b
truejojo <johannesveh@yahoo.de>
2023-03-01 12:01:36 +0100

    delete some absolute path

--- src/pages/host/components/HostVan.tsx
+++ src/pages/host/components/HostVan.tsx
@@ -5,12 +5,12 @@ const HostVan = ({ id, name, imageUrl, price }: IVanProps) => {
   return (
     <div className="bg-neutral-100 p-24">
       <div className="flex">
-        <NavLink to={`/host/vans/${id}`}>
+        <NavLink to={id}>
           <img src={imageUrl} alt={name} style={{ width: "100px" }} />
         </NavLink>
         <div>
           <h2 className="fw-bold">
-            <NavLink to={`/host/vans/${id}`}>{name}</NavLink>
+            <NavLink to={id}>{name}</NavLink>
           </h2>
           <p>${price}/day</p>
         </div>
--- src/pages/van/components/Van.tsx
+++ src/pages/van/components/Van.tsx
@@ -3,15 +3,14 @@ import { Link } from "react-router-dom";
 export interface IVanProps {
   id: string;
   name: string;
-  description: string;
   imageUrl: string;
   type: string;
   price: number;
 }
 
-const Van = ({ id, name, description, imageUrl, type, price }: IVanProps) => {
+const Van = ({ id, name, imageUrl, type, price }: IVanProps) => {
   return (
-    <Link to={`/vans/${id}`} className="card">
+    <Link to={id} className="card">
       <img src={imageUrl} alt={name} />
       <div className="flex-space-between">
         <h3 className="fw-bold">{name}</h3>
